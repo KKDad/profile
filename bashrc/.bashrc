6610b5bc222aa9006e636335f7ed215e12eb5f79
Adrian Gilbert <adrian@gilbert.ca>
2020-05-25 07:57:55 -0400

    Initial commit

---
+++ bashrc/.bashrc
@@ -0,0 +1,8 @@
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+fi
+
+source ~/.bashrc/colors.sh
+source ~/.bashrc/prompt.sh
+source ~/.bashrc/aws.sh
---
+++ bashrc/prompt.sh
@@ -0,0 +1,28 @@
+find_git_branch() {
+  # Based on: http://stackoverflow.com/a/13003854/170413
+  local branch
+  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+    if [[ "$branch" == "HEAD" ]]; then
+      branch='detached*'
+    fi
+    git_branch="($branch)"
+  else
+    git_branch=""
+  fi
+}
+
+find_git_dirty() {
+  local status=$(git status --porcelain 2> /dev/null)
+  if [[ "$status" != "" ]]; then
+    git_dirty='*'
+  else
+    git_dirty=''
+  fi
+}
+
+PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+
+export PS1="\n$txtgrn\u@\h $txtylw\w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\n$txtgrn└─[$txtrst\]\$ "
+
+# Default Git enabled root prompt (for use with "sudo -s")
+#export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
