740c6cb32df4729b572d3857f57cb8d2a11566ab
Adrian Gilbert <adrian@gilbert.ca>
2026-02-21 14:19:38 -0500

    Switch nvm default from node 20 to latest LTS

--- env/zsh/nvm_setup.sh
+++ env/zsh/nvm_setup.sh
@@ -15,4 +15,4 @@ export NVM_DIR="$HOME/.nvm"
 # This loads nvm bash_completion
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" 
 
-nvm use 20
+nvm use --lts
